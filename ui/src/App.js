bf701c65e865da491735dbcd41bfbf7fde933c14
Ananthan R <ananthan.rajasekharan@gmail.com>
2021-12-29 06:46:14 +0530

    done..done

--- ui/src/App.js
+++ ui/src/App.js
@@ -20,14 +20,15 @@ import NFTMarket from "./ContractJSON/NFTMarket.json";
 import NFT from "./ContractJSON/NFT.json";
 
 function App() {
-
   useEffect(() => {
     getColorTokenDetails();
+    getItemDetails();
     enableMetaMask();
   }, []);
 
   const [nftValue, setnftValue] = useState("");
   const [colorTokenDetails, setColorTokenDetails] = useState([]);
+  const [itemDetails, setItemDetails] = useState([]);
 
   const ethereum = window.ethereum;
 
@@ -35,16 +36,18 @@ function App() {
 
   const NFTMarketContractAddress = NFTMarket.networks["5777"].address;
   const NFTMarketContractAbi = NFTMarket.abi;
-  const NFTMarketContract = new web3.eth.Contract(NFTMarketContractAbi, NFTMarketContractAddress);
+  const NFTMarketContract = new web3.eth.Contract(
+    NFTMarketContractAbi,
+    NFTMarketContractAddress
+  );
 
   const NFTContractAddress = NFT.networks["5777"].address;
   const NFTContractAbi = NFT.abi;
   const NFTContract = new web3.eth.Contract(NFTContractAbi, NFTContractAddress);
 
-
   const nftValueChangeHandler = (event) => {
     setnftValue(event.target.value);
-  };  
+  };
 
   const enableMetaMask = async () => {
     await ethereum.request({ method: "eth_requestAccounts" });
@@ -80,12 +83,58 @@ function App() {
     setColorTokenDetails(colorTokenDetailsList);
   };
 
+  const putOnSale = async (id) => {
+    console.log(id);
+    enableMetaMask();
+    const infoValue = await NFTMarketContract.methods
+      .createMarketItem(NFTContractAddress, id)
+      .send({ from: ethereum.selectedAddress, value: 25000000000000000 });
+    console.log(infoValue);
+    getItemDetails();
+  };
+
+  const getItemDetails = async () => {
+    const itemCount = await NFTMarketContract.methods.getItemCount().call();
+    console.log(itemCount);
+    let itemDetailsList = [];
+    for (var i = 1; i <= itemCount; i++) {
+      const itemDetail = await NFTMarketContract.methods
+        .idToMarketItem(i)
+        .call();
+      const colorNFTValue = await NFTContract.methods
+        .colorsName(itemDetail.tokenId)
+        .call();
+      console.log(itemDetail);
+      if (!itemDetail.sold) {
+        let newItemToken = {
+          colorTokenValue: colorNFTValue,
+          nftSeller: itemDetail.seller,
+          itemID: i,
+        };
+        itemDetailsList.push(newItemToken);
+      }
+    }
+    console.log(itemDetailsList);
+    setItemDetails(itemDetailsList);
+  };
+
+  const buyToken = async (id) => {
+    console.log(id);
+    enableMetaMask();
+    const infoValue = await NFTMarketContract.methods
+      .createMarketSale(NFTContractAddress, id)
+      .send({ from: ethereum.selectedAddress, value: 1000000000000000000 });
+    console.log(infoValue);
+    getItemDetails();
+    getColorTokenDetails();
+  };
+
   return (
     <div style={{ maxWidth: "99.20%" }}>
       <Navbar bg="light" expand="sm">
         <Container fluid>
           <Navbar.Brand href="/">
-            <b>Color Token Collection</b>
+            <b>🛒</b>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
 
@@ -95,9 +144,12 @@ function App() {
               placeholder="Hex Color Code"
               className="me-2"
               aria-label="hexCode"
+              value={nftValue}
               onChange={nftValueChangeHandler}
             />
-            <Button variant="outline-primary" onClick={mintTokenHandler}>Mint Token</Button>
+            <Button variant="outline-primary" onClick={mintTokenHandler}>
+              Mint Token
+            </Button>
           </Form>
         </Container>
       </Navbar>{" "}
@@ -114,22 +166,30 @@ function App() {
           style={{ paddingLeft: "10px" }}
         >
           <Row xs={1} md={4} className="g-4">
-
-          {colorTokenDetails.map((colorDetail) => (
+            {colorTokenDetails.map((colorDetail) => (
               <Col>
                 <Card style={{ width: "20rem" }}>
                   <Card.Header
-                    style={{ backgroundColor: colorDetail.colorTokenValue, height: "3rem" }}
+                    style={{
+                      backgroundColor: colorDetail.colorTokenValue,
+                      height: "3rem",
+                    }}
                   ></Card.Header>
                   <Card.Body>
-                    <Card.Title style={{ fo: "#000000" }}>
+                    <Card.Title>
                       <b> {colorDetail.colorTokenValue} </b>
                     </Card.Title>
 
                     <Card.Text>
-                      0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+                      <b>Owner:</b> {colorDetail.nftOwner} <br />
+                      <b>NFT ID:</b> {colorDetail.nftID}
                     </Card.Text>
-                    <Button variant="warning">Sell Token</Button>
+                    <Button
+                      variant="warning"
+                      onClick={() => putOnSale(colorDetail.nftID)}
+                    >
+                      Sell Token
+                    </Button>
                   </Card.Body>
                 </Card>
               </Col>
@@ -143,31 +203,25 @@ function App() {
           style={{ paddingLeft: "10px" }}
         >
           <Row xs={1} md={4} className="g-4">
-            {[
-              "#fcba03",
-              "#75d147",
-              "#ffffff",
-              "#000000",
-              "#f20707",
-              "#025173",
-              "#7de374",
-              "#e174e3",
-              "#5a045c",
-            ].map((variant, idx) => (
+            {itemDetails.map((itemDetail) => (
               <Col>
-                <Card style={{ width: "20rem" }} key={idx}>
+                <Card style={{ width: "20rem" }}>
                   <Card.Header
-                    style={{ backgroundColor: variant, height: "3rem" }}
+                    style={{
+                      backgroundColor: itemDetail.colorTokenValue,
+                      height: "3rem",
+                    }}
                   ></Card.Header>
                   <Card.Body>
-                    <Card.Title style={{ fo: "#000000" }}>
-                      <b> {variant} </b>
+                    <Card.Title>
+                      <b> {itemDetail.colorTokenValue} </b>
                     </Card.Title>
 
                     <Card.Text>
-                      0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+                      <b>Seller: </b> {itemDetail.nftSeller} <br />
+                      <b>Price: </b> 1 Ether.
                     </Card.Text>
-                    <Button variant="info">Buy Token</Button>
+                    <Button variant="info" onClick={() => buyToken(itemDetail.itemID)}>Buy Token</Button>
                   </Card.Body>
                 </Card>
               </Col>
